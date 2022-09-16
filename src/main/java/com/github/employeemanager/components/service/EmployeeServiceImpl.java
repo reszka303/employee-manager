5b52f965b81dc4927cba07d25cb946a4d47a4439
reszka303 <reszka303@op.pl>
2022-09-16 20:04:02 +0200

    add method getAllEmployess() and add test for this method
    add EmployeeService, EmployeeServiceImpl, EmployeeNotFoundException, EmployeeServiceImplTest

---
+++ src/main/java/com/github/employeemanager/components/service/EmployeeServiceImpl.java
@@ -0,0 +1,60 @@
+package com.github.employeemanager.components.service;
+
+import com.github.employeemanager.components.domain.Employee;
+import com.github.employeemanager.components.dto.request.EmployeeRequestDto;
+import com.github.employeemanager.components.dto.response.EmployeeResponseDto;
+import com.github.employeemanager.components.exception.EmployeeNotFoundException;
+import com.github.employeemanager.components.mapper.EmployeeMapper;
+import com.github.employeemanager.components.repository.EmployeeRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicReference;
+
+@Service
+@Transactional
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private final EmployeeRepository repository;
+
+    public EmployeeServiceImpl(EmployeeRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public List<EmployeeResponseDto> getAllEmployees() {
+        return repository.findAll().stream().map(EmployeeMapper::toResponse).toList();
+    }
+
+    @Override
+    public EmployeeResponseDto findEmployeeById(Long id) {
+
+        Optional<Employee> byId = repository.findById(id);
+        byId.orElseThrow( () -> new EmployeeNotFoundException("No employee with " + id + " id"));
+        Employee employee = byId.get();
+
+        return EmployeeMapper.toResponse(employee);
+    }
+
+    @Override
+    public EmployeeResponseDto findEmployeeByName(String name) {
+        return null;
+    }
+
+    @Override
+    public EmployeeResponseDto addEmployee(EmployeeRequestDto request) {
+        return null;
+    }
+
+    @Override
+    public EmployeeResponseDto updateEmployee(EmployeeRequestDto request) {
+        return null;
+    }
+
+    @Override
+    public void deleteEmployee(Long id) {
+
+    }
+}
---
+++ src/test/java/com/github/employeemanager/components/service/EmployeeServiceImplTest.java
@@ -0,0 +1,211 @@
+package com.github.employeemanager.components.service;
+
+import com.github.employeemanager.components.domain.Employee;
+import com.github.employeemanager.components.dto.EmployeeBaseDto;
+import com.github.employeemanager.components.dto.request.EmployeeRequestDto;
+import com.github.employeemanager.components.dto.response.EmployeeResponseDto;
+import com.github.employeemanager.components.repository.EmployeeRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class EmployeeServiceImplTest {
+
+    @Mock
+    private EmployeeRepository repository;
+
+    @InjectMocks
+    private EmployeeServiceImpl service;
+
+    private Employee employee;
+
+    private EmployeeRequestDto request;
+
+    private EmployeeResponseDto response;
+
+
+    private EmployeeRequestDto getRequest() {
+
+        return EmployeeRequestDto.builder()
+                .id(1L)
+                .name("Lucas Samweyes")
+                .email("lsamweyes0@fda.com")
+                .phone("300-245-4509")
+                .imageUrl("https://www.bootdey.com/app/webroot/img/Content/avatar/avatar7.png")
+                .employeeCode("2f0d3f6a-136c-49f5-8d4b-16ede1578873")
+                .build();
+    }
+
+    @AfterEach
+    public void cleanUp() {
+        repository.deleteAll();
+    }
+
+    @Test
+    public void shouldReturnListAllEmployeeResponseDtoWhenEmployeesAre() {
+
+        //given
+        List<Employee> list = getListEmployee();
+//        when(repository.findAll()).thenReturn(list);
+        given(repository.findAll()).willReturn(list);
+
+        //when
+        List<EmployeeResponseDto> result = service.getAllEmployees();
+
+        //then
+        System.out.println(result);
+        assertThat(result.get(2).getName()).isEqualTo("James Cowderoy");
+    }
+
+    private List<Employee> getListEmployee() {
+
+        Employee lucasSamweyes =
+                new Employee(
+                        1L,
+                        "Lucas Samweyes",
+                        "lsamweyes0@fda.com",
+                        "Senior Full-Stack Developer",
+                        "300-245-4509",
+                        "https://www.bootdey.com/app/webroot/img/Content/avatar/avatar7.png",
+                        "2f0d3f6a-136c-49f5-8d4b-16ede1578873"
+                );
+
+        Employee oliverSkerratt =
+                new Employee(
+                        2L,
+                        "Oliver Skerratt",
+                        "oskerratt@fda.com",
+                        "Senior Java Developer",
+                        "363-977-1893",
+                        "https://www.bootdey.com/app/webroot/img/Content/avatar/avatar5.png",
+                        "a346cce8-c33f-427b-884d-8a4d113bd164"
+                );
+
+        Employee jamesCowderoy =
+                new Employee(
+                        3L,
+                        "James Cowderoy",
+                        "jcowderoy@fda.com",
+                        "Chief Executive Officer",
+                        "955-985-2702",
+                        "https://www.bootdey.com/img/Content/avatar/avatar1.png",
+                        "bec4f978-7c05-45a4-b1c4-6bc8e457ad17"
+                );
+
+        Employee barrieRockcliffe =
+                new Employee(
+                        4L,
+                        "Barrie Rockcliffe",
+                        "brockcliffe@fda.com",
+                        "Senior Front-End Developer",
+                        "490-351-2899",
+                        "https://www.bootdey.com/app/webroot/img/Content/avatar/avatar6.png",
+                        "22dea798-e979-4393-afd4-f76765893c4e"
+                );
+
+        Employee krystleFeasley =
+                new Employee(
+                        5L,
+                        "Krystle Feasley",
+                        "kfeasley@fda.com",
+                        "Secretary",
+                        "473-436-5477",
+                        "https://www.bootdey.com/app/webroot/img/Content/avatar/avatar3.png",
+                        "0f82f41d-aff7-40a8-8842-0b5c46f57763"
+                );
+
+        List<Employee> list = new ArrayList<>();
+
+        list.add(lucasSamweyes);
+        list.add(oliverSkerratt);
+        list.add(jamesCowderoy);
+        list.add(barrieRockcliffe);
+        list.add(krystleFeasley);
+
+        return list;
+    }
+
+    private List<EmployeeResponseDto> getListEmployeeResponseDto() {
+
+        EmployeeResponseDto lucasSamweyes =
+                new EmployeeResponseDto(
+                        1L,
+                        "Lucas Samweyes",
+                        "lsamweyes0@fda.com",
+                        "Senior Full-Stack Developer",
+                        "300-245-4509",
+                        "https://www.bootdey.com/app/webroot/img/Content/avatar/avatar7.png",
+                        "2f0d3f6a-136c-49f5-8d4b-16ede1578873"
+                );
+
+        EmployeeResponseDto oliverSkerratt =
+                new EmployeeResponseDto(
+                        2L,
+                        "Oliver Skerratt",
+                        "oskerratt@fda.com",
+                        "Senior Java Developer",
+                        "363-977-1893",
+                        "https://www.bootdey.com/app/webroot/img/Content/avatar/avatar5.png",
+                        "a346cce8-c33f-427b-884d-8a4d113bd164"
+                );
+
+        EmployeeResponseDto jamesCowderoy =
+                new EmployeeResponseDto(
+                        3L,
+                        "James Cowderoy",
+                        "jcowderoy@fda.com",
+                        "Chief Executive Officer",
+                        "955-985-2702",
+                        "https://www.bootdey.com/img/Content/avatar/avatar1.png",
+                        "bec4f978-7c05-45a4-b1c4-6bc8e457ad17"
+                );
+
+        EmployeeResponseDto barrieRockcliffe =
+                new EmployeeResponseDto(
+                        4L,
+                        "Barrie Rockcliffe",
+                        "brockcliffe@fda.com",
+                        "Senior Front-End Developer",
+                        "490-351-2899",
+                        "https://www.bootdey.com/app/webroot/img/Content/avatar/avatar6.png",
+                        "22dea798-e979-4393-afd4-f76765893c4e"
+                );
+
+        EmployeeResponseDto krystleFeasley =
+                new EmployeeResponseDto(
+                        5L,
+                        "Krystle Feasley",
+                        "kfeasley@fda.com",
+                        "Secretary",
+                        "473-436-5477",
+                        "https://www.bootdey.com/app/webroot/img/Content/avatar/avatar3.png",
+                        "0f82f41d-aff7-40a8-8842-0b5c46f57763"
+                );
+
+        List<EmployeeResponseDto> list = new ArrayList<>();
+
+        list.add(lucasSamweyes);
+        list.add(oliverSkerratt);
+        list.add(jamesCowderoy);
+        list.add(barrieRockcliffe);
+        list.add(krystleFeasley);
+
+        return list;
+    }
+
+}
