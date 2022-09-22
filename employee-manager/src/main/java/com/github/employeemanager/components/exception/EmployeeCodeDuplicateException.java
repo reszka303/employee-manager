a67ecfe381353d6f728f3ed6242535224a1139ef
reszka303 <reszka303@op.pl>
2022-09-23 00:36:58 +0200

    remove unnecessary folder

--- employee-manager/src/main/java/com/github/employeemanager/components/exception/EmployeeCodeDuplicateException.java
+++
@@ -1,9 +0,0 @@
-package com.github.employeemanager.components.exception;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(value = HttpStatus.CONFLICT, reason = "The same employee code already exists")
-public class EmployeeCodeDuplicateException extends RuntimeException {
-
-}
